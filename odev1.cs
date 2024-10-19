e7a0294dbdba81f7e74ee8bf24f506bfc1990cfe
Betül Yıldız <betulyilddiz@gmail.com>
2024-10-19 23:10:16 +0300

    Create odev1.cs

---
+++ odev1.cs
@@ -0,0 +1,119 @@
+﻿/*
+// iki sayının farkını bulan C# kodu
+int sayi1 = 9;
+int sayi2 = 4;
+int fark = sayi1 - sayi2;
+Console.WriteLine("İki sayının farkı: " + fark);
+Console.ReadKey();
+*/
+
+
+
+/*
+// 5 sayısının karesini bulan C# kodu
+int sayi = 5;
+int kare = sayi * sayi;
+Console.WriteLine("5 sayısının karesi: " + kare);
+Console.ReadKey();
+*/
+
+
+/*
+// 10 sayısının 3'e bölümünden kalanını bulan C# kodu
+int sayi1 = 10;
+int sayi2 = 3;
+int kalan = sayi1 % sayi2;  
+Console.WriteLine("10'un 3'e bölümünden kalan: " + kalan);
+Console.ReadKey();
+*/
+
+
+
+/*
+// klavyeden girilen 4 sayının toplamını bulan C# kodu
+int sayi1, sayi2, sayi3, sayi4;
+
+Console.WriteLine("1. sayıyı girin:");
+sayi1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("2. sayıyı girin:");
+sayi2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("3. sayıyı girin:");
+sayi3 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("4. sayıyı girin:");
+sayi4 = Convert.ToInt32(Console.ReadLine());
+
+int toplam = sayi1 + sayi2 + sayi3 + sayi4;
+Console.WriteLine("Sayıların toplamı: " + toplam);
+Console.ReadKey();
+*/
+
+
+
+/*
+// klavyeden girilen 4 sayının çarpımını bulan C# kodu
+int sayi1, sayi2, sayi3, sayi4;
+
+Console.WriteLine("1. sayıyı girin:");
+sayi1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("2. sayıyı girin:");
+sayi2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("3. sayıyı girin:");
+sayi3 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("4. sayıyı girin:");
+sayi4 = Convert.ToInt32(Console.ReadLine());
+
+int carpim = sayi1 * sayi2 * sayi3 * sayi4;
+Console.WriteLine("Sayıların çarpımı: " + carpim);
+Console.ReadKey();
+*/
+
+
+/*
+// klavyeden kenarları girilen dikdörtgenler prizmasının hacmini hesaplayan C# kodu
+
+int uzun_kenar, kisa_kenar, yukseklik;
+
+Console.WriteLine("Uzun kenarı giriniz:");
+uzun_kenar = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Kısa kenarı giriniz:");
+kisa_kenar = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Yüksekliği giriniz:");
+yukseklik = Convert.ToInt32(Console.ReadLine());
+
+int hacim = uzun_kenar * kisa_kenar * yukseklik;
+Console.WriteLine("Dikdörtgenler prizmasının hacmi: " + hacim);
+Console.ReadKey();
+*/
+
+
+
+/*
+// klavyeden girilen  iki sayının bölümünü bulan C# kodu
+int sayi1,sayi2;
+
+Console.WriteLine("1. sayıyı girin:");
+sayi1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("2. sayıyı girin:");
+sayi2 = Convert.ToInt32(Console.ReadLine());
+
+int bolum = sayi1 / sayi2;
+Console.WriteLine("sayi1'in sayi2'ye bölümü: " + bolum);
+Console.ReadKey();
+*/
+
+
+
+
+
+
+
+
